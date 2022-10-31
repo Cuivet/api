7f4f13a13831a89016da6401d12ab78cb7b3778b
martina.barella <martinabarella98@gmail.com>
2022-10-30 22:45:15 -0300

    fix: hairColors route

--- routes/api/hair_color.route.js
+++ routes/api/hair_color.route.js
@@ -3,7 +3,7 @@ const hairColorService = require('../../services/hair_color.service');
 
 router.get('/all', async (req,res) => {
     console.log('Request to ' + req.method + ' on: ' + req.baseUrl + req.url);
-    const hairColors = await drugService.findAll();
+    const hairColors = await hairColorService.findAll();
     res.json(hairColors);
 });
 
