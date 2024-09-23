072a07b7b9ea0ddf67aaedc4603eefe92960b851
FrattinJuan <frattin.juan9@gmail.com>
2024-09-23 16:41:58 -0300

    fix: CUIVET123-21 Boton ver datos clinica

--- seeders/20220910172049-species.js
+++ seeders/20220910172049-species.js
@@ -6,6 +6,15 @@ module.exports = {
       {id:1, name: 'Perro', createdAt: new Date(), updatedAt: new Date()},
       {id:2, name: 'Gato', createdAt: new Date(), updatedAt: new Date()},
       {id:3, name: 'Ave', createdAt: new Date(), updatedAt: new Date()},
+      {id: 4, name: 'Conejo', createdAt: new Date(), updatedAt: new Date()},
+      {id: 5, name: 'Hámster', createdAt: new Date(), updatedAt: new Date()},
+      {id: 6, name: 'Cobayo', createdAt: new Date(), updatedAt: new Date()},
+      {id: 7, name: 'Reptil', createdAt: new Date(), updatedAt: new Date()},
+      {id: 8, name: 'Pez', createdAt: new Date(), updatedAt: new Date()},
+      {id: 9, name: 'Hurón', createdAt: new Date(), updatedAt: new Date()},
+      {id: 10, name: 'Caballo', createdAt: new Date(), updatedAt: new Date()},
+      {id: 11, name: 'Cerdo', createdAt: new Date(), updatedAt: new Date()},
+      {id: 12, name: 'Vaca', createdAt: new Date(), updatedAt: new Date()},
     ]
 
     return queryInterface.bulkInsert('specie',species,{} );
